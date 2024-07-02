da05fc94e3aa30f092b5cf81758bf536926d7ab3
nanase <sgmdq601@gmail.com>
2024-07-02 21:58:32 +0900

    mac用の切り替え機能を追加

--- fc980c/keymaps/myKeymap/keymap.c
+++ fc980c/keymaps/myKeymap/keymap.c
@@ -14,6 +14,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#ifdef CONSOLE_ENABLE
+    #include <print.h>
+#endif    
 #include QMK_KEYBOARD_H
 #define MY_TAPPING_TERM 40
 
@@ -36,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NUM, KC_INS, KC_PGUP, KC_PGDN,
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_P7, KC_P8, KC_P9, KC_PPLS,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_P4, KC_P5, KC_P6,
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_P4, KC_P5, KC_P6,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_P1, KC_P2, KC_P3, KC_PENT,
-        KC_LCTL, KC_LGUI, KC_F13, KC_SPC, KC_F14 , KC_RCTL, KC_F24, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_PDOT)
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT, KC_F24, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_PDOT)
 };
 
 static bool kc2flg = false;
@@ -62,7 +65,22 @@ static bool alt = false;
 static bool isPressedFn = false;
 static bool isMac = false;
 
+#ifdef CONSOLE_ENABLE
+    void keyboard_post_init_user(void) {
+    // Customise these values to desired behaviour
+    debug_enable=true;
+     //debug_matrix=true;
+    //debug_keyboard=true;
+    //debug_mouse=true;
+}
+#endif
+
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    #ifdef CONSOLE_ENABLE
+        uprintf("KL: kc: %u¥n", keycode);
+    #endif
+
+
     switch(keycode){
         case KC_F24:
             isPressedFn = record->event.pressed;
@@ -70,9 +88,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         case KC_W:
             if(isPressedFn) {
                 if (record->event.pressed) {
-                    layer_on(0);
+                    layer_off(2);
                     isMac = false;
                 }
+                return false;
             }
         case KC_M:
             if(isPressedFn) {
@@ -80,6 +99,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                     layer_on(2);
                     isMac = true;
                 }
+                return false;
             }
     }
 
